d471c80bdbfcb888e5c200a5e452836e870d4ead
Bartek <bl4kee@wp.pl>
2021-03-14 00:23:18 +0100

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameFormComponent } from './name-form/name-form.component';
 import { FormsModule } from '@angular/forms';
+import { GreetingComponent } from './greeting/greeting.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NameFormComponent
+    NameFormComponent,
+    GreetingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
